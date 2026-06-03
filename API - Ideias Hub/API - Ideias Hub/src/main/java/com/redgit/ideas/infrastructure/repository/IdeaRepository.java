e8bd7b4eec7284cdac3387edccdebc3930fd27a5
Pradixx <dieguittosilva09@gmail.com>
2026-06-03 15:39:26 -0300

    refactor(ideas): adicionar assinatura paginada em IdeaRepository

--- API - Ideias Hub/API - Ideias Hub/src/main/java/com/redgit/ideas/infrastructure/repository/IdeaRepository.java
+++ API - Ideias Hub/API - Ideias Hub/src/main/java/com/redgit/ideas/infrastructure/repository/IdeaRepository.java
@@ -1,12 +1,12 @@
 package com.redgit.ideas.infrastructure.repository;
 
 import com.redgit.ideas.infrastructure.entities.Idea;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface IdeaRepository extends MongoRepository<Idea, String> {
-    List<Idea> findByAuthorId(String authorId);
+    Page<Idea> findByAuthorId(String authorId, Pageable pageable);
 }

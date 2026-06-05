2488c31bf1097a699f6fb2dd661410cf923eed39
Pradixx <dieguittosilva09@gmail.com>
2026-06-05 12:32:52 -0300

    refactor(reputation): atualizar UserReputationRepository para buscar por userId

--- API - Reputation/src/main/java/com/daniel/registry/reputation/infrastructure/repository/UserReputationRepository.java
+++ API - Reputation/src/main/java/com/daniel/registry/reputation/infrastructure/repository/UserReputationRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserReputationRepository extends JpaRepository<UserReputation, Long> {
-    Optional<UserReputation> findByUserEmail(String userEmail);
+    Optional<UserReputation> findByUserId(String userId);
 }

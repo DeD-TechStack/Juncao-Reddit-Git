dc8aace6ccf34484a9e8d9ad58c402e0edd99249
Pradixx <dieguittosilva09@gmail.com>
2026-06-05 12:29:18 -0300

    fix(reputation): habilitar @EnableRetry na aplicação principal

--- API - Reputation/src/main/java/com/daniel/registry/reputation/ReputationServiceApplication.java
+++ API - Reputation/src/main/java/com/daniel/registry/reputation/ReputationServiceApplication.java
@@ -2,8 +2,10 @@ package com.daniel.registry.reputation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class ReputationServiceApplication {
 
 	public static void main(String[] args) {

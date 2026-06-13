3af77b73892fbaa8753fdcaa6293809606041193
Pradixx <dieguittosilva09@gmail.com>
2026-06-13 20:04:26 -0300

    refactor(ideias-hub): trocar MongoRepository por JpaRepository

--- Backend/API - Ideias Hub/API - Ideias Hub/src/main/java/com/redgit/ideas/infrastructure/repository/IdeaRepository.java
+++ Backend/API - Ideias Hub/API - Ideias Hub/src/main/java/com/redgit/ideas/infrastructure/repository/IdeaRepository.java
@@ -3,10 +3,10 @@ package com.redgit.ideas.infrastructure.repository;
 import com.redgit.ideas.infrastructure.entities.Idea;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IdeaRepository extends MongoRepository<Idea, String> {
+public interface IdeaRepository extends JpaRepository<Idea, String> {
     Page<Idea> findByAuthorId(String authorId, Pageable pageable);
 }
